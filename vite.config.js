704f08570a3958c4bb714c4a01ef568ec5f85cba
Yannaev <69728833+yannaev@users.noreply.github.com>
2026-03-05 17:03:02 +0500

    init vite with tailwind and build

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+    ],
+    build: {
+        outDir: 'docs',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: 'index.html'
+            }
+        }
+    }
+})
